5ba2a9e0544ce7ab0467edd02980f5029025a59d
danielchp <eddypineda135@gmail.com>
2020-10-08 07:14:14 +0000

    tarea2 modified

--- tarea2.py
+++ tarea2.py
@@ -1,10 +1,11 @@
 horas=int(input("cuanto tiempo trabajo por dia"))
 sueldo=int(input("cuanto le pagan por hora"))
 horasS=horas*7
-if horasS<=40 :
+if horasS>40 :
        horax=horasS-40
        sueldoF=(horasS*sueldo)+(horax*sueldo)
-       print("tu pago semanal es")
-else:
+       print(f"tu pago semanal es .",sueldoF)
+else :
+    horas<40
     sueldoF=(horasS*sueldo)
-    print(" tu sueldo es de")
+    print(" tu sueldo es de:",sueldoF)
