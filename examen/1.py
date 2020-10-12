5f427787c995b5185a15a925bab3dcccbce2fa97
danielchp <eddypineda135@gmail.com>
2020-10-12 00:27:50 +0000

    examen/1.py modified

---
+++ examen/1.py
@@ -0,0 +1,15 @@
+def  BonoParaDocentesEDCHP ():
+    #Datos de entrada
+    str
+puntos=int(input("Digite el valor de puntos:"))
+salario=int(input("Digite el valor de calario mini:"))
+#proceso
+if puntos>=0 and puntos<=100 :
+ monto=salario*0.1
+else :
+ puntos>=101 and puntos<=150 
+monto=salario*0.5
+if puntos>=151 :
+  monto=salario*0.8 
+print("El monto del bono del profesor es :",monto)
+BonoParaDocentesEDCHP
