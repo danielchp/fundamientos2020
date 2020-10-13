f7e87de014f0f238e735531d86778e81de4e400d
danielchp <eddypineda135@gmail.com>
2020-10-13 01:33:54 +0000

    examen/11.py new file

---
+++ examen/11.py
@@ -0,0 +1,15 @@
+def PromedioFinal():
+    #datos de entrada
+    str
+    nota1=int(input("ingresar notas:"))
+    nota2=int(input("ingresar nota"))
+    nota3=int(input("ingresar nota"))
+    nota4=int(input("ingresar notas"))
+    #proceso
+    U1=nota1*0.1
+    U2=nota2*0.15
+    U3=nota3*0.25
+    U4=nota4*0.5
+    c = U1+U2+U3+U4
+    print("tu promedio es:",c)
+PromedioFinal()
