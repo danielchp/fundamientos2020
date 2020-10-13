98219488d141b1344429e099d35ecfd998622a80
danielchp <eddypineda135@gmail.com>
2020-10-13 13:58:23 +0000

    modified

--- examen/01.py
+++ examen/01.py
@@ -1,15 +1,15 @@
-def PromedioFinal():
+def PromedioFinalEDCHP():
     #datos de entrada
     str
-    nota1=int(input("ingresar notas:"))
+    nota1=int(input("ingresar nota:"))
     nota2=int(input("ingresar nota"))
     nota3=int(input("ingresar nota"))
-    nota4=int(input("ingresar notas"))
+    nota4=int(input("ingresar nota"))
     #proceso
     U1=nota1*0.1
     U2=nota2*0.15
     U3=nota3*0.25
     U4=nota4*0.5
-    c = U1+U2+U3+U4
-    print("tu promedio es:",c)
-PromedioFinal()
+    promedio = U1+U2+U3+U4
+    print("tu promedio es:",promedio)
+PromedioFinalEDCHP()
---
+++ practicaENclase/practica1.py
@@ -0,0 +1,11 @@
+def sumarnumero():
+    #ingresando variables
+    contadorcondicion,numero, suma=0,0,0
+    #proceso y datos de entrada
+    while contadorcondicion<=10
+    numero=float(input(F"ingresar el numero de la {contadorcondicion}"))
+    suma=suma+numero
+    contadorcondicion=contadorcondicion+1
+    print(f"la suma de los numero es{suma}")
+sumarnumero():
+    
