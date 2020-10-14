bbffe45a8be81fb7172eec5174928c1e9e43e936
danielchp <eddypineda135@gmail.com>
2020-10-14 00:42:06 +0000

    new file

---
+++ examen/05.py
@@ -0,0 +1,8 @@
+def calcularSalarioEDCHP():
+    str
+print("inicio")
+salario=1400
+for i in [2,3,4,5,6,7]:
+     print(f"Hola.la cantidad de años es {i} y tu salario es {salario*100/100}")
+     salario=salario*1.1
+calcularSalarioEDCHP()
